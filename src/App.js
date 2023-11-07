c9a16a1711dea4972ec4db76b2e5a4db9828a9cf
Urmas K <urmas.kryner@gmail.com>
2023-11-07 21:17:11 +0000

    deleted ExpenseItem visual

--- src/App.js
+++ src/App.js
@@ -46,17 +46,13 @@ const App = () => {
                     <div className = 'col-sm'>
                         <ExpenseList/>
                     </div>
-                    <p></p><div className= 'col-sm'>
-                        <ExpenseItem/>
-                    </div>
                 </div> 
                 <h3 className='mt-3'> Change allocation</h3>
                 <div className='row mt-3'>
                     <div className= 'col-sm'>
                         <AllocationForm/>
                     </div>
-                </div>
-                <div><CustomDropdown/></div>                       
+                </div>                   
             </div>             
         </AppProvider>
     );
